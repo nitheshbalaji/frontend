b7fffe08ed5b12aa582a2372b67b0a7297de63bc
Aparajhitha <aparaar62@gmail.com>
2025-12-30 19:19:08 +0530

    Update CitizenLayout.jsx

--- src/layouts/CitizenLayout.jsx
+++ src/layouts/CitizenLayout.jsx
@@ -1,17 +1,15 @@
 import Topbar from "../components/Topbar";
-import CitizenSidebar from "../components/CitizenSidebar";
 import { Outlet } from "react-router-dom";
 
 export default function CitizenLayout() {
   return (
     <div className="app-layout">
       <Topbar />
-      <div className="layout-body">
-        <CitizenSidebar />
-        <main className="content-area">
-          <Outlet />
-        </main>
-      </div>
+
+      {/* Full-width content, NO sidebar */}
+      <main className="content-area full-width">
+        <Outlet />
+      </main>
     </div>
   );
 }
