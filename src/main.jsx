26ee4f7234787a5ff597c54d3afcd59a50b5aa7d
Aparajhitha <aparaar62@gmail.com>
2025-12-30 13:07:33 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { AuthProvider } from "./context/AuthContext";
 import { ComplaintProvider } from "./context/ComplaintContext";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <HashRouter>
       <AuthProvider>
