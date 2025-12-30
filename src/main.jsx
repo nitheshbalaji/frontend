9751d8ec724a67143a726884a9683b74e3c37143
Aparajhitha <aparaar62@gmail.com>
2025-12-30 13:05:13 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { ComplaintProvider } from "./context/ComplaintContext";
-
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <ComplaintProvider>
           <App />
         </ComplaintProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
