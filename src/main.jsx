008eb58f1a6796488168ca2a34486e513ab93ee1
Aparajhitha <aparaar62@gmail.com>
2025-12-30 12:49:36 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { ComplaintProvider } from "./context/ComplaintContext";
 
 import "./styles.css";
 
@@ -10,7 +11,9 @@ createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ComplaintProvider>
+          <App />
+        </ComplaintProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
