9fb52ec35f4836c8c7de2ee9cee07c410c00e375
Aparajhitha <aparaar62@gmail.com>
2025-12-30 13:22:10 +0530

    Update PageWrappper.jsx

--- src/components/common/PageWrappper.jsx
+++ src/components/common/PageWrappper.jsx
@@ -1,11 +1,1 @@
-export default function PageWrapper({ children }) {
-  return (
-    <div style={{
-      minHeight: "100vh",
-      background: "linear-gradient(135deg,#eef2ff,#f8fafc)",
-      padding: "30px"
-    }}>
-      {children}
-    </div>
-  );
-}
+
