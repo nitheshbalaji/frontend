fdf64582cef67b9514c26f5233fe07158e521e76
Aparajhitha <aparaar62@gmail.com>
2025-12-30 21:42:19 +0530

    Update Dashboard.jsx

--- src/pages/Citizen/Dashboard.jsx
+++ src/pages/Citizen/Dashboard.jsx
@@ -1,7 +1,10 @@
 import "../../styles/dashboard.css";
-import BoardColumn from "../../components/dashboard/BoardColumn";
 import StatMiniCard from "../../components/dashboard/StatMiniCard";
 import TicketsChart from "../../components/dashboard/TicketsChart";
+import StatCard from "../../components/dashboard/StatCard";
+import HeatMap from "../../components/dashboard/HeatMap";
+import TrendChart from "../../components/dashboard/TrendChart";
+import RecentComplaints from "../../components/dashboard/RecentComplaints";
 
 export default function Dashboard() {
   return (
